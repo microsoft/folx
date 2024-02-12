fix jacobian of slogdet

folx/wrapped_functions.py
@@ -146,7 +146,7 @@ def slogdet_jvp(primals, tangents):
     jacobians = jnp.linalg.inv(primals)
 
     def custom_jvp(jacobian, tangent, sign):
-        jac_dot_tangent = jnp.vdot(jacobian.T, tangent)
+        jac_dot_tangent = jnp.vdot(jacobian.T.conj(), tangent)
         if jac_dot_tangent.dtype in (jnp.complex64, jnp.complex128):
             sign_jvp = sign * -1j * jac_dot_tangent.imag
             log_det_jvp = jac_dot_tangent.real

test/test_layers.py
@@ -94,12 +94,7 @@ class TestForwardLaplacian(LaplacianTestCase):
                 self.assert_allclose(y.jacobian.dense_array, jac)
                 self.assert_allclose(y.laplacian, self.laplacian(f, (a, b)))
 
-    @parameterized.expand(
-        [
-            (False,),
-            # (True,)
-        ]
-    )
+    @parameterized.expand([(False,), (True,)])
     def test_slogdet(self, test_complex: bool):
         x = np.random.normal(size=(16 * 16))
         w = np.random.normal(size=(16, 16))