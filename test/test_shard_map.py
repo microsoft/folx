4800eaab5aba0612f5885e791a5d167a631d916f
Clemens Giuliani <clemens@inailuig.it>
2025-11-22 01:07:21 +0100

    add regression test

---
+++ test/test_shard_map.py
@@ -0,0 +1,24 @@
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+
+from folx import forward_laplacian
+
+
+def test_shard_map_bug_integer_pow():
+    # see https://github.com/microsoft/folx/issues/38
+
+    def f(w, x):
+        return jax.lax.integer_pow(x @ w, 1)
+
+    @jax.smap(out_axes=0, in_axes=(None, 0), axis_name='i')
+    @partial(jax.vmap, in_axes=(None, 0))
+    def test(w, x):
+        return forward_laplacian(partial(f, w))(x)
+
+    x = jnp.ones((1, 16))
+    w = jnp.ones((16, 16))
+
+    with jax.set_mesh(jax.sharding.Mesh(jax.devices(), 'i')):
+        test(w, x)
